13b880bcbdfac90ccfce24bf9383327cdf314b14
이혜원 <dahxtq1@naver.com>
2025-09-08 01:25:37 +0900

    [BE] wip: SecurityConfig 구현 중

---
+++ backend-spring/src/main/java/com/bukadong/tcg/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.bukadong.tcg.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 인증/인가 환경 설정 Configuration 클래스
+ *
+ */
+@Configuration
+public class SecurityConfig {
+}
