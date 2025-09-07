90e08436abde6f2cdea4924f1051c83bebd16b80
이혜원 <dahxtq1@naver.com>
2025-09-07 16:43:29 +0900

    [BE] fix: 주석 오타 해결 및 redis value 직렬화 방법 변경

--- backend-spring/src/main/java/com/bukadong/tcg/config/RedisConfig.java
+++ backend-spring/src/main/java/com/bukadong/tcg/config/RedisConfig.java
@@ -11,6 +11,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -53,11 +54,11 @@ public class RedisConfig {
         RedisTemplate<String, Object> t = new RedisTemplate<>();
         t.setConnectionFactory(redisConnectionFactory());
 
-        // 식렬화 방법 설정
+        // 직렬화 방법 설정
         t.setKeySerializer(new StringRedisSerializer());
-        t.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        t.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         t.setHashKeySerializer(new StringRedisSerializer());
-        t.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        t.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         // 트랜잭션 설정
         t.setEnableTransactionSupport(true);
