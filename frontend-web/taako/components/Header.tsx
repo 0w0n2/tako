3fdba4bf9ba21a70297c3185903d050650593195
Jodndud <doriconi@gmail.com>
2025-09-08 17:53:07 +0900

    feat: Header, SearchInput컴포넌트 구현완료

--- frontend-web/taako/components/Header.tsx
+++ frontend-web/taako/components/Header.tsx
@@ -1,7 +1,31 @@
+import SearchInput from "./atoms/Input/SearchInput";
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <div className="default-container">
-        <h1>Taako</h1>
+    <div className="border-b border-[#353535]">
+      <div className="default-container flex justify-between items-center py-6">
+        <div className="flex gap-10">
+          <div className="flex items-center"><a href="/"><h1>TAAKO</h1></a></div>
+          <SearchInput />
+        </div>
+        <ul className="category-wrap flex gap-10 items-center">
+          <li>
+            <div>
+              <a href="#" className="flex items-center gap-2">
+                카테고리
+              <div><Image src="/icon/arrow-down.svg" width="8" height="4" alt="arrow-down" /></div>
+              </a>
+            </div>
+            </li>
+          <li><a href="#" className="">미니게임</a></li>
+          <li>로그인</li>
+          <li><a href="/mypage" className="">마이페이지</a></li>
+          <li><a href="/notification" className="">알림</a></li>
+          <li><a href="/auction/new" className="px-10 py-4 bg-gray-700 text-white cursor-pointer rounded-md bg-gradient-to-r from-[#863BA9] to-[#487BD9]">경매등록</a></li>
+        </ul>
+      </div>
+      
     </div>
   );
 }
--- frontend-web/taako/components/atoms/Input/SearchInput.tsx
+++ frontend-web/taako/components/atoms/Input/SearchInput.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function SearchInput() {
     return (
-        <div>
-            <input type="text" placeholder="Search" />
+        <div className="relative">
+            <input
+                className="w-[350px] rounded-full border-1 border-[#353535] bg-[#191924] px-6 py-4"
+                type="text"
+                placeholder="검색어를 입력해주세요."
+            />
+            <Image src="/icon/search.svg" className="absolute right-6 top-1/2 -translate-y-1/2" width="15" height="15" alt="" />
         </div>
     );
 }
