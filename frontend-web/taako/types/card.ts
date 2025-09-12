6f339af8f1635d196aa6eb11fb631156d0966d80
OneArmedofLepanto <high.kick.jake@gmail.com>
2025-09-12 12:20:41 +0900

    [FE] refactor: cardLIst Component 삭제 및 TCGcard 컴포넌트 확장장

--- frontend-web/taako/types/card.ts
+++ frontend-web/taako/types/card.ts
@@ -1,10 +1,21 @@
 export interface Card {
-    id: number;
-    name: string;
-    grade: string;
-    rarity: string;
-    categoryMajorId: number;
-    categoryMajorName: string;
-    categoryMediumId: number;
-    categoryMediumName: string,
+  id: number;
+  name: string;
+  grade: string;
+  rarity: string;
+  categoryMajorId: number;
+  categoryMajorName: string;
+  categoryMediumId: number;
+  categoryMediumName: string,
+}
+
+export const CARD_SIZE = {
+  YuGiOh: { width: 59, height: 86 },
+  Pokemon: { width: 63, height: 88 },
+  MTG: { width: 63, height: 88 },
+} as const;
+
+export type CharacterCardProps = {
+  imageUrl: string,
+  cardType: keyof typeof CARD_SIZE
 }
---
+++ frontend-web/taako/types/category.ts
@@ -0,0 +1,5 @@
+export interface CategoryPageProps {
+  params: {
+    category_name: string
+  }
+}
