04ceaabc23a10a54bcd55932b4e5a1522a75eb4d
hyewon <dahxtq1@naver.com>
2025-09-16 01:26:39 +0900

    [BC] feat: 프로젝트 초기 구조 세팅 완료

--- backend-solidity/hardhat.config.ts
+++ backend-solidity/hardhat.config.ts
@@ -1,8 +1,39 @@
-import { HardhatUserConfig } from "hardhat/config";
+import "dotenv/config";
+
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+
+if (!SEPOLIA_RPC_URL || !SEPOLIA_PRIVATE_KEY) {
+  console.error(".env 파일에 SEPOLIA_URL과 SEPOLIA_PRIVATE_KEY를 설정해주세요.");
+  process.exit(1); 
+}
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+      sepolia: {
+        url: SEPOLIA_RPC_URL,
+        accounts:[SEPOLIA_PRIVATE_KEY],
+      },
+    },
+    gasReporter: {
+      enabled: process.env.REPORT_GAS !== "true",
+      currency: "USD",
+    },
+    etherscan: {
+      apiKey: process.env.ETHERSCAN_API_KEY,
+    },
 };
 
 export default config;
