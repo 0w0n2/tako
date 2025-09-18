b01343fe05eb4e44fca075d75bbbd7d996ed51d4
hyewon <dahxtq1@naver.com>
2025-09-19 08:58:17 +0900

    [BE] chore: 스Swagger 설명 추가

--- backend-spring/src/main/java/com/bukadong/tcg/api/member/controller/MyWalletController.java
+++ backend-spring/src/main/java/com/bukadong/tcg/api/member/controller/MyWalletController.java
@@ -6,6 +6,7 @@ import com.bukadong.tcg.api.member.service.MemberQueryService;
 import com.bukadong.tcg.api.member.service.MemberWalletService;
 import com.bukadong.tcg.global.common.base.BaseResponse;
 import com.bukadong.tcg.global.security.dto.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Member")
+@Tag(name = "Member", description = "회원 전용 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/members/me/wallet")
@@ -24,6 +25,7 @@ public class MyWalletController {
     private final MemberWalletService memberWalletService;
     private final MemberQueryService memberQueryService;
 
+    @Operation(summary = "신규 지갑 주소 등록", description = "지갑 주소가 없는 사용자 계정에 메타마스크 또는 EVM 서비스의 지갑 주소를 연동합니다.")
     @PostMapping()
     public BaseResponse<Void> linkWallet(
             @Valid @RequestBody WalletLinkRequestDto requestDto,
