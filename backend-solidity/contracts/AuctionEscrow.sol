5b08a47311d04044edd19fe31af47ac0c5991bc0
hyewon <dahxtq1@naver.com>
2025-09-18 11:36:24 +0900

    [BC] feat: 경매 에스크로 기능 구현

--- backend-solidity/contracts/AuctionEscrow.sol
+++ backend-solidity/contracts/AuctionEscrow.sol
@@ -11,7 +11,7 @@ pragma solidity ^0.8.28;
     4. 판매자가 예치된 대금을 인출 (releaseFunds)
  */
 contract AuctionEscrow {
-    // --- 상태 변수 정의 ---
+    // --- 상태 변수, Enum ---
     address public seller; // 판매자 주소
     address public buyer; // 구매자 주소
     uint256 public amount; // 거래액 (낙찰가)
@@ -25,28 +25,32 @@ contract AuctionEscrow {
     }
     State public currentState; // 현재 거래 상태
 
-    // --- 이벤트 정의 ---
+    // --- 이벤트 ---
     event Deposited(address indexed buyer, uint256 amount); // 입금 완료 이벤트
     event ReceiptConfirmed(address indexed buyer);
     event FundsReleased(address indexed seller, uint256 amount); // 대금 정산 완료 이벤트
-    event Cancled(address indexed canceller); // 거래 취소 이벤트
+    event Canceled(address indexed canceller); // 거래 취소 이벤트
+
+    // -- 커스텀 에러 --
+    error NotSeller();
+    error NotBuyer();
+    error InvalidState();
+    error IncorrectAmount();
+    error TransferFailed();
 
     // --- 제어자 정의 ---
     modifier onlyBuyer() {
-        require(msg.sender == buyer, "Only buyer can call this function.");
+        if (msg.sender != buyer) revert NotBuyer();
         _;
     }
 
     modifier onlySeller() {
-        require(msg.sender == seller, "Only seller can call this function.");
+        if (msg.sender != seller) revert NotSeller();
         _;
     }
 
     modifier inState(State _state) {
-        require(
-            currentState == _state,
-            "This function be called in the current state."
-        );
+        if (currentState != _state) revert InvalidState();
         _;
     }
 
@@ -64,7 +68,7 @@ contract AuctionEscrow {
     }
 
     /**
-        @dev [구매자] 낙찰 대금을 컨트랙트에 입금(결제)
+        @dev [구매자] 대금 입금(결제)
      */
     function deposit()
         external
@@ -72,14 +76,14 @@ contract AuctionEscrow {
         onlyBuyer
         inState(State.AwaitingPayment)
     {
-        require(msg.value == amount, "Incorrect deposit amount");
+        if (msg.value != amount) revert IncorrectAmount();
 
         currentState = State.AwaitingConfirmation;
         emit Deposited(buyer, msg.value);
     }
 
     /**
-        @dev [구매자] 상품을 수령했음을 확인
+        @dev [구매자] 상품 수령 확인
      */
     function confirmReceipt()
         external
@@ -91,13 +95,13 @@ contract AuctionEscrow {
     }
 
     /**
-        @dev [판매자] 예치된 대금을 자신의 지갑으로 인출 (Pull-over-Push 패턴 적용)
+        @dev [판매자] 대금 인출
      */
     function releaseFunds() external onlySeller inState(State.Complete) {
         // 상태를 먼저 변경하여 Re-entrancy 공격 방지
-        (bool success, ) = seller.call{value: amount}("");
-        require(success, "Failed to send funds to seller");
-
         emit FundsReleased(seller, amount);
+
+        (bool success, ) = seller.call{value: amount}("");
+        if (!success) revert TransferFailed();
     }
 }
