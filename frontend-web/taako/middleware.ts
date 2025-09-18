676e4527cfd023edda96aad8990f78ee713d9060
Jodndud <doriconi@gmail.com>
2025-09-18 16:29:59 +0900

    feat: 엑세스 토큰API 위한 test페이지

--- frontend-web/taako/middleware.ts
+++ frontend-web/taako/middleware.ts
@@ -1,22 +1,24 @@
-import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get("accessToken")?.value
+  const token = req.cookies.get("Authorization")?.value;
 
-  if (!token && (
-    req.nextUrl.pathname.startsWith("/mypage")
-    // 다른 페이지 추가
-  )){
-    return NextResponse.redirect(new URL("/", req.url))
+  const protectedPaths = [
+    "/mypage",
+    // 추가 라우트가드
+  ];
+
+  if (!token && protectedPaths.some((path) => req.nextUrl.pathname.startsWith(path))) {
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-        "/mypage/:path*",
-        // 다른 페이지 추가
-    ],
-}
+  matcher: [
+    "/mypage/:path*",
+    // 추가 라우트가드
+  ],
+};
