93f333d387d456cddd699f3f02fcabc3ea21dbac
jodndud <doriconi@gmail.com>
2025-09-18 21:01:27 +0900

    feat: category type수정

--- frontend-web/taako/hooks/useMajorCategories.ts
+++ frontend-web/taako/hooks/useMajorCategories.ts
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { getMajorCategories } from "@/lib/category";
+import { MajorCategories } from "@/types/category";
 
 export function useMajorCategories() {
-  const [majorCategories, setMajorCategories] = useState([]);
+  const [majorCategories, setMajorCategories] = useState<MajorCategories[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
