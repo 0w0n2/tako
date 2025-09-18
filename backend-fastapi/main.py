d2d8f61461487d01fe09e8f9ff65b115773a089d
jungseok yu <9526yu@naver.com>
2025-09-18 17:16:38 +0900

    [BE] refactor: uvicorn 구동 cmd 변경

--- backend-fastapi/main.py
+++ backend-fastapi/main.py
@@ -390,4 +390,4 @@ async def condition_check(
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:condition_check", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
