95c234059ea09e3ea77e1facbe89516ec7c6bd83
jodndud <doriconi@gmail.com>
2025-09-22 02:42:29 +0900

    feat: 메인페이지 UI진행중

--- frontend-web/taako/app/page.tsx
+++ frontend-web/taako/app/page.tsx
@@ -1,11 +1,30 @@
-import MainInfoSection from "@/components/sections/MainInfoSection";
+import MainBanner from "@/components/sections/MainBanner";
+import MainHotCardSection from "@/components/sections/MainHotCardSection";
+import MainItemEndCloseSection from "@/components/sections/MainItemEndCloseSection";
 import MainItemListSection from "@/components/sections/MainItemListSection";
 
 export default function Home() {
   return (
     <div>
-      <MainInfoSection />
-      <MainItemListSection />
+      <MainBanner />
+
+      {/* 인기 카테고리 */}
+      <MainHotCardSection />
+
+      {/* 마감 임박 경매 */}
+      <MainItemEndCloseSection />
+
+      {/* 전체 카테고리 */}
+
+      {/* 포켓몬, 유희왕 경매 */}
+      <div className="default-container pt-30">
+        <h2>진행중인 포켓몬 경매 {`>`}</h2>
+        <MainItemListSection id={3} />
+      </div>
+      <div className="default-container pt-30">
+        <h2>진행중인 유희왕 경매 {`>`}</h2>
+        <MainItemListSection id={1} />
+      </div>
     </div>
   );
 }
