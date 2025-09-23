a5cac64d77692d7d562948cbf33023ce34be2665
Jodndud <doriconi@gmail.com>
2025-09-23 17:54:06 +0900

    feat: 유저사이드바에 기능 구현

---
+++ frontend-web/taako/hooks/useMyInfo.ts
@@ -0,0 +1,33 @@
+// hooks/useMyInfo.ts
+import { useQuery } from "@tanstack/react-query";
+import { getInfo, getMyBidAuction } from "@/lib/mypage";
+import { MyInfo, MyBidAuctions } from "@/types/auth";
+
+export function useMyInfo() {
+  // 내 프로필 조회
+  const {
+    data: myInfo, isLoading: myInfoLoading, error: myInfoError,
+  } = useQuery<{ result: MyInfo }>({
+    queryKey: ["myInfo"],
+    queryFn: getInfo,
+  });
+
+  // 내 입찰 경매 조회
+  const {
+    data: myBidAuctionsData, isLoading: myBidLoading, error: myBidError,
+  } = useQuery<{ result: { content: MyBidAuctions[] } }>({
+    queryKey: ["myBidAuctions"],
+    queryFn: getMyBidAuction,
+  });
+  // 안전하게 배열 반환
+  const myBidAuctions = myBidAuctionsData?.result?.content ?? [];
+
+  return {
+    myInfo: myInfo?.result ?? null,
+    myInfoLoading,
+    myInfoError,
+    myBidAuctions,
+    myBidLoading,
+    myBidError,
+  };
+}
--- frontend-web/taako/types/auth.ts
+++ frontend-web/taako/types/auth.ts
@@ -6,3 +6,35 @@ export interface AuthState {
     refreshAccessToken: () => Promise<string | null>;
     logout: () => void;
   }
+
+  // 내 프로필 조회
+export interface MyInfo {
+  backgroundImageUrl: string|null;
+  email: string|null;
+  introduction: string|null;
+  mumberId: number;
+  nickname: number;
+  notificationsSetting: Object|null;
+  profileImageUrl: string|null;
+}
+
+// 내 입찰 경매 목록 조회
+export interface MyBidAuctions {
+  auctionId: number;
+  code: string|null;
+  title: string|null;
+  startDatetime: string|null;
+  endDatetime: string|null;
+  isEnd: boolean|null;
+  closeReason: string|null;
+  currentPrice: number|null;
+  myTopBidAmount: number|null;
+  imageUrl: string|null;
+  bids: [
+    {
+      time: string|null;
+      nickname: string|null;
+      price: number|null;
+    }
+  ]
+}
