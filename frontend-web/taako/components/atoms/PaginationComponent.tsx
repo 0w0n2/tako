4e9ff7b68e960091935b25d06250d04fc9bef454
jodndud <doriconi@gmail.com>
2025-09-25 00:25:21 +0900

    feat: pagination구현, 총갯수 수정

--- frontend-web/taako/components/atoms/PaginationComponent.tsx
+++ frontend-web/taako/components/atoms/PaginationComponent.tsx
@@ -1,31 +1,65 @@
+"use client";
+
+import { useSearchParams, useRouter } from "next/navigation";
 import {
   Pagination,
   PaginationContent,
-  PaginationEllipsis,
   PaginationItem,
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
-} from "@/components/ui/pagination"
+} from "@/components/ui/pagination";
 
-import { PageProps } from "@/types/auction";
+interface PaginationComponentProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
 
-export default function PaginationComponent() {
+export default function PaginationComponent({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: PaginationComponentProps) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i);
+
+  const handlePageChange = (page: number) => {
+    onPageChange(page);
+
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("page", String(page));
+    router.push(`/search?${params.toString()}`);
+  };
+
   return (
     <Pagination>
       <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious href="#" />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext href="#" />
-        </PaginationItem>
+        {currentPage > 0 && (
+          <PaginationItem>
+            <PaginationPrevious size="md" onClick={() => handlePageChange(currentPage - 1)} />
+          </PaginationItem>
+        )}
+
+        {pageNumbers.map((num) => (
+          <PaginationItem key={num}>
+            <PaginationLink
+               size="md"
+              onClick={() => handlePageChange(num)}
+              className={currentPage === num ? "font-bold" : ""}
+            >
+              {num + 1}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+
+        {currentPage < totalPages - 1 && (
+          <PaginationItem>
+            <PaginationNext size="md" onClick={() => handlePageChange(currentPage + 1)} />
+          </PaginationItem>
+        )}
       </PaginationContent>
     </Pagination>
   );
