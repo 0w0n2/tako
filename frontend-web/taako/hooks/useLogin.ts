e0cb3621c2c7074d35b881b1b524fcc2003f2b85
OneArmedofLepanto <high.kick.jake@gmail.com>
2025-09-26 10:24:57 +0900

    [ FE] 인증 로직 오류 수정

--- frontend-web/taako/hooks/useLogin.ts
+++ frontend-web/taako/hooks/useLogin.ts
@@ -1,19 +1,22 @@
 'use client';
 import { useState } from "react";
 import { useAuthStore } from "@/stores/useAuthStore";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export function useLogin() {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const { login, logout } = useAuthStore();
     const router = useRouter();
+    const searchParams = useSearchParams();
 
     const handleLogin = async () => {
         try {
             const success = await login(email, password);
             if (success) {       // 성공일 때만 이동
-                router.push("/");
+                // 리다이렉트 파라미터가 있으면 해당 경로로, 없으면 홈으로
+                const redirectPath = searchParams.get('redirect') || '/';
+                router.push(redirectPath);
             }
         } catch (err: any) {
             console.log(err.message);
