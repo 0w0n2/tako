1a55ae2a7bbfb9e5db005f4f234eb811fcab32ac
jungseok yu <9526yu@naver.com>
2025-09-28 03:43:27 +0900

    [FE] refactor: NFT 소유권 이전 승인 여부 메세지 수정

--- frontend-web/taako/components/nft/SellerPanel.tsx
+++ frontend-web/taako/components/nft/SellerPanel.tsx
@@ -113,7 +113,7 @@ export default function SellerPayoutPanel({
       )}
 
       {!nftNotMinted && alreadyApproved && (
-        <div className="text-xs text-green-400 mt-1">이미 승인됨</div>
+        <div className="text-xs text-green-400 mt-1">NFT 소유권 이전 권한 승인 완료!</div>
       )}
 
       <div className="flex flex-col md:flex-row gap-3">
