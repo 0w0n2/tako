575d763a95df004a690af68402542e79efe500ae
김민규(KIM MINGYU) <jun3021303@naver.com>
2025-09-29 03:23:26 +0900

    [FE] 판매자 페이지 이미지 url 연결

--- frontend-web/taako/types/seller.ts
+++ frontend-web/taako/types/seller.ts
@@ -1,12 +1,13 @@
 import { AuctionCardProps } from "./auction";
 
 export interface Seller {
-    id: number;
-    nickname: string;
-    reviewCount: number;
-    reviewStarAvg: number;
-    profileImageUrl: string;
-    introduction: string;
-    trustScore: number;
-    sellAuctions: AuctionCardProps[];
+	id: number;
+	nickname: string;
+	reviewCount: number;
+	reviewStarAvg: number;
+	profileImageUrl: string | null;
+	backgroundImageUrl?: string | null;
+	introduction: string;
+	trustScore: number;
+	sellAuctions: AuctionCardProps[];
 }
